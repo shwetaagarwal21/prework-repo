180e05441e3a4c5cb53d3b2da76348012320b11f
shwetaagarwal21 <shweta.agarwal21@gmail.com>
2018-05-25 23:48:01 -0700

    Create IdenticalBinaryTrees.java

---
+++ binary-tree/IdenticalBinaryTrees.java
@@ -0,0 +1,26 @@
+/*
+Identical Binary Trees
+*/
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int isSameTree(TreeNode A, TreeNode B) {
+        if(A == null && B == null) return 1;
+        if(A == null || B == null) return 0;
+        if(A.val != B.val)
+            return 0;
+        else 
+            return ((isSameTree(A.left, B.left) == 1) && (isSameTree(A.right, B.right) == 1))? 1 : 0;
+    }
+}
